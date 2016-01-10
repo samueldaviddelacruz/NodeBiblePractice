0fb5a34f9716545bd18ddca676bff79e656a317d
Samuel David De La Cruz <alphaelena@gmail.com>
2016-01-09 23:35:45 -0400

    -added Loading icon when retrieving data, added font awesome and bootstrap,got a better top Menu

--- public/js/BibleApp.js
+++ public/js/BibleApp.js
@@ -4,25 +4,26 @@ app = angular.module('bibleApp',[]);
 var bibleController = function($http,$sce){
 console.log('angular ready to rumble')
 var bible = this;
-
+bible.IsLoadingVerses=false;
 
  bible.renderHtml = function (htmlCode) {
             return $sce.trustAsHtml(htmlCode);
         };
 bible.ListVerses = function(chapter){
+bible.selectedChapter = chapter;
+console.log('Chapter selected');
 
-console.log('book selected')
-
+    bible.IsLoadingVerses = true;
 $http.get('/api/verses/'+chapter.id).then(function(result){
-chapter.verses = result.data.response.verses;
-debugger;
-
+    bible.selectedChapter.verses = result.data.response.verses;
+    bible.IsLoadingVerses = false;
 })
 
 }
 
 $http.get('/api/books').then(function(result){
 bible.books = result.data.response.books;
+    bible.selectedBook = bible.books[0];
 debugger;
 
 })
@@ -30,7 +31,7 @@ debugger;
 
 
 
-}
+};
 
 
 
